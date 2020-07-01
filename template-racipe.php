b4b85852df720732a6d9120ff1f9e9a3dcda704f
Luan Ardrizzo <luan.ardrizzo@gmail.com.br>
2020-07-01 15:32:38 -0300

    Clonando estrutura - RAC

---
+++ template-racipe.php
@@ -0,0 +1,480 @@
+
+<?php 
+
+/* Template name: Template Racipe */
+
+get_header(); ?>
+
+<meta charset="UTF-8">
+
+
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>-child/css/style.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>-child/css/new-style.css">
+	
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+
+
+<body>
+	
+	<div class="rac-pagina">
+	
+
+		<section id="section__home">
+
+
+
+			<div class="chamada">
+
+				<div class="chamada__top">
+				
+					<div class="chamada__top--left">
+						RAC
+					</div>
+
+					<div class="chamada__top--right">				
+					
+						<div class="right__top">				
+							REVISÃO AVANÇADA
+						</div>
+						
+						<div class="right__bottom">				
+							DE <b>CLÍNICA MÉDICA</b>
+						</div>	
+
+					</div>
+
+				</div>
+
+
+				<div class="chamada__bottom">
+					iMED
+				</div>
+
+
+			</div>
+
+
+			<!-- <img class="medeletro__logo" src="<?php bloginfo('template_directory'); ?>-child/imgs/medeletro-negativo.svg" alt=""> -->
+
+			
+			
+			<h1>A Revisão Avançada é um programa especial, elaborado para aqueles que desejam aumentar ainda mais o seu diferencial. O objetivo é o aprofundamento em temas usualmente recomendados para estudo em casa (pela baixa frequência em provas), mas que vêm ganhando cada vez mais importância, especialmente nos concursos mais recentes. São dois módulos independentes e distintos do INTENSIVÃO.</h1>
+
+			
+			<a href="#section__infosProduto" id="nextSection"><img src="<?php bloginfo('template_directory'); ?>-child/imgs/scroll-mouse-medeletro.gif" alt=""></a>
+		</section>
+	
+	
+		<section id="section__infosProduto">
+				
+			<div class="container-medeletro">
+	
+				<!-- <img  data-aos="zoom-in"  data-aos-delay="500"  data-aos-duration="1000" src="<?php bloginfo('template_directory'); ?>-child/imgs/imagem-totalmente-diferente-dos-cursos-tradicionais.gif" alt="" class="infosProdutoGif"> -->
+				
+	
+	
+			<div class="section__animation" data-aos-delay="700"  data-aos-duration="500" data-aos="fade-left">
+				<h2>FORMATO iMed</h2>
+	
+				<div class="section___row">
+					<div class="box__infosProduto">
+						<div class="icone">
+							<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icon-responsive.svg" alt="" class="icone__responsive">
+						</div>
+						<div class="texto">
+							<span class="large">100% on-line</span>
+							<p>(app MEDSOFT Pro)</p>
+						</div>
+					</div>
+	
+					<div class="box__infosProduto">
+						<div class="icone">
+							<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-reader.svg" alt="" class="icone__reader">
+						</div>
+						<div class="texto">
+							<span class="large">3 apostilas digitais</span>
+							<p>+ de 200 vídeos entremeados<br>no material</p>
+						</div>
+					</div>
+	
+					<div class="box__infosProduto">
+						<div class="icone">
+							<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-calendario.svg" alt="" class="icone__calendario">
+						</div>
+						<div class="texto">
+							<span class="large">3 apostila DIGITAL</span>
+							<p><span class="large">por semana</span><br>(3 semanas)</p>
+						</div>
+					</div>
+				</div>
+			</div>
+	
+			<p>O conteúdo baixado no dispositivo de cada assinante poderá ser acessado mesmo após o encerramento da distribuição de conteúdo.</p>
+	
+			</div>
+	
+		</section>
+	
+		<section id="section__temas">
+			
+			<div class="container-medeletro">
+	
+				<h1>TEMAS</h1>
+	
+				<div class="linha">
+					
+					<div class="coluna">
+						<ul class="items__temas">
+							<li>Diálise e Transplante</li>
+							<li>Ventilação Mecânica</li>
+							<li>Prescrição Médica</li>
+							<li>Geriatria</li>
+							<li>Desordens dos nervos cranianos</li>
+						</ul>
+					</div>
+	
+	
+					<div class="coluna">
+						<ul class="items__temas">
+							<li>Trombose de seios durais</li>
+							<li>Doenças desmielinizantes</li>
+							<li>Oncologia</li>
+							<li>Infecções associadas ao cateter venoso central</li>
+							<li>Tabagismo</li>
+						</ul>
+					</div>
+	
+				</div>
+			
+			
+			
+			</div>
+	
+		</section>
+	
+		<section id="section__valores">
+			<div class="container-medeletro">	
+				<h1>VALORES E FORMA DE PAGAMENTO</h1>
+				<div class="box-container">
+
+					<div class="column">
+						<h2>Preço promocional para
+							contratantes do Intensivão 2020</h2>
+						<div class="box__valores">
+							<div class="titulo">
+								<h5>À vista</h5>
+							</div>
+							<div class="texto">
+								<p class="medio">R$368,00</p>
+								<p>no boleto bancário</p>
+							</div>
+						</div>
+						<div class="box__valores">
+							<div class="titulo">
+								<h5>Cheque pós datato</h5>
+							</div>
+							<div class="texto">
+								<p class="medio">R$ 648,00</p>
+								<p>parcela única, datada<br>
+									para 15/02/2020*</p>
+							</div>
+						</div>
+						<p>*Sujeito a aprovação</p>
+					</div>
+
+					<div class="column">
+						<span class="separator"></span>
+					</div>
+
+					<div class="column">
+						<h2>Preço para demais contratantes
+							à vista no boleto bancário</h2>
+						<div class="box__valores">
+							<div class="titulo">
+								<h5>Já fez ou faz algum programa
+									preparatório do MEDGRUPO</h5>
+							</div>
+							<div class="texto">
+								<p class="medio">R$ 1.938,00</p>
+							</div>
+						</div>
+						<div class="box__valores">
+							<div class="titulo">
+								<h5>Novo no MEDGRUPO</h5>
+							</div>
+							<div class="texto">
+								<p class="medio">R$ 2.638,00</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<p class="paragrafo">Ao comprar o módulo on-line <i><b>MEDELETRO IMED</b></i>, você estará adquirindo (embora com pagamento parcelado) a <u>COLEÇÃO COMPLETA</u>, que será disponibilizada pelo APP em 11 etapas semanais sequenciais. Após o encerramento, você mantém todo o conteúdo das apostilas digitais já baixadas em seu dispositivo.</p>
+			</div>
+		</section>	
+	
+		<section id="section__imedFeitoSobMedidaParaOSeuObjetivo">
+			
+			<div class="container-medeletro">
+				
+				<h1>COMO FUNCIONA O FORMATO iMED?</h1>
+	
+				<div class="section__row">
+					
+					<div class="section__column">
+						<p class="p1">O <b><i>iMED</i></b> é o resultado da união das didáticas oral (aula) e escrita (texto) em um único formato: o <u>eletrônico</u>. Os contratantes receberão material acadêmico único, na forma de <u>textos especiais</u>, extremamente completos e detalhados, e ainda assim preservando a didática. Como? Não serão textos quaisquer, pois os escritores – os próprios – estarão presentes dentro do conteúdo eletrônico, dando voz às páginas, em inserções constantes e estrategicamente premeditadas.</p>		
+	
+						<img class="ipad-texto desktop" src="<?php bloginfo('template_directory'); ?>-child/imgs/ipad-medeletro-texto.png" alt="" class="imgImedFeitoSobMedidaParaOSeuObjetivo">
+	
+						<img class="ipad-texto mobile" src="<?php bloginfo('template_directory'); ?>-child/imgs/ipad-medeletro-texto-responsive.png" alt="" class="imgImedFeitoSobMedidaParaOSeuObjetivo">
+					</div>
+	
+					<div class="section__column">
+						<img class=" devices-comofunciona desktop" src="<?php bloginfo('template_directory'); ?>-child/imgs/mock-tablet-desktop.png" alt="">	
+	
+						<img class=" devices-comofunciona mobile" src="<?php bloginfo('template_directory'); ?>-child/imgs/ipad-medeletro-img.png" alt="" class="imgImedFeitoSobMedidaParaOSeuObjetivo">
+					</div>
+	
+				</div>
+	
+				<h4 class="mt-40">AINDA NÃO ENTENDI...</h4>
+	
+				<p><u><b>Como apostilas conseguem “falar”, sua leitura nunca será solitária ou desestimulante.</b></u></p>
+	
+				<p>Existirão sempre 3 personagens:</p>
+	
+				<div class="box-container">
+						
+					<div id="box__imedFeitoSobMedidaParaOSeuObjetivo1" class="box__imedFeitoSobMedidaParaOSeuObjetivo">
+						<div class="icone">
+							<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-doutor.svg" alt="" class="icone__voce">
+						</div>
+						<div class="texto">
+							<p><b>Você</b></p>
+						</div>
+					</div>
+	
+					<div id="box__imedFeitoSobMedidaParaOSeuObjetivo2" class="box__imedFeitoSobMedidaParaOSeuObjetivo">
+						<div class="icone">
+							<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-livro-eletronico.png" alt="" class="icone__livroEletronico">
+						</div>
+						<div class="texto">
+							<p>O <b>livro eletrônico</b></p>
+							<p class="reduzido">(trazendo textos extremamente completos e detalhados)</p>
+						</div>
+					</div>
+	
+					<div id="box__imedFeitoSobMedidaParaOSeuObjetivo3" class="box__imedFeitoSobMedidaParaOSeuObjetivo">
+						<div class="icone">
+							<img src="<?php bloginfo('template_directory'); ?>-child/imgs/o-escritor-2.svg" alt="" class="icone__oEscritorQueOsConfeccionou">
+						</div>
+						<div class="texto">
+							<p><b>O escritor</b> que os<br>confeccionou</p>
+							<p class="reduzido">(na função de orientador, de Coach)</p>
+						</div>
+					</div>
+	
+				</div>
+	
+				<p class="texto-corrido">Os escritores não só redigirão o material (textos), mas estarão nele de fato, em vídeo, segurando a mão de cada um dos leitores e os conduzindo à sua leitura.</p>
+	
+				<p class="texto-corrido">Eles darão estímulo, ânimo, incentivo; estarão presentes e atuantes nos parágrafos mais complexos, simplificando os conceitos, chamando a atenção para passagens importantes do texto (que você em outras circunstâncias poderia não se atentar com o rigor necessário), ou mesmo te antecipando a explicação de conceitos dos parágrafos subsequentes, eventualmente fundamentais para a compreensão do assunto.</p>
+	
+				<p class="texto-corrido">Enfim, com o <b><i>iMED</i></b> seu treinamento será <u>direcionado</u> e <u>eficiente</u>. Você estudará por textos, sim, mas sua leitura será “supervisionada” pelos próprios escritores, que serão seus tutores e amigos por um longo período; verdadeiros guias para a sua aprovação!</p>
+	
+			</div>
+	
+		</section>
+	
+	
+	
+	
+	
+	
+		<!-- -->
+		<!-- -->
+		<!-- ATUANDO AQUI -->
+		<!-- -->
+		<!-- -->
+	
+	
+	
+		<section id="section__informacoesTecnicas">
+			
+			<div class="container-medeletro">
+				
+	
+				<h1>INFORMAÇÕES TÉCNICAS</h1>
+				
+				
+				<div class="box__informacoesTecnicas">
+					<div class="coluna__imagem">
+						<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-moble-desktop-tablet-ipad.png" alt="">
+					</div>
+	
+					<div class="coluna__texto">
+						<!-- Tabs -->
+						<div class='toggle'>
+						  <div class='tabs'>
+							
+							<div class='tab active'>
+								<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-windows.svg" alt=""><span class="windows">Windows</span>
+							</div>
+						   
+							<div class='tab'>
+								<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-macOS.svg" alt=""><span class="macOS">macOS</span>
+							</div>
+	
+							<div class='tab'><img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-dispositivosMoveis.svg" alt="">
+								<span class="dispositivosMoveis">Dispositivos móveis</span>
+							</div>
+						  </div>
+	
+						  <!-- Panels -->
+						  <div class='panels'>
+	
+							<!-- Panel: Windows -->
+							<div class='panel'>
+								<h4>REQUISITOS MÍNIMOS</h4>
+								<hr>
+								<p><b>Sistema Operacional:</b> Windows 10 64 bits.</p>
+								<p><b>Processador:</b> Frequência de no minimo 2.0 GHz(GigaHertz) ou superior.</p>
+								<p><b>Memória:</b> 2 GB (GigaBytes) de memoria RAM ou superior.</p>
+								<p><b>Espaço Disponível em Disco:</b> 5 GB(GigaBytes) Livres de espaço em disco ou superior.</p>
+								<p><b>Velocidade de Internet:</b>  Internet banda larga com velocidade de 1 megabit (taxa de transferência disponível para download de 200 kilobits /seg).</p>
+								<a href="https://medsoft-desktop-update.s3.amazonaws.com/medsoft-pro.exe" class="btn_informacoesTecnicas">Download</a>
+							</div>
+							<!-- Fim Panel: Windows -->
+	
+							<!-- Panel: macOS -->
+							<div class='panel'>
+								<h4>REQUISITOS MÍNIMOS</h4>
+								<hr>
+								<p><b>Sistema Operacional:</b> MacOS X 10.13 64 bits ou superior</p>
+								<p><b>Processador:</b> Frequência de no mínimo 2.0 GHz(GigaHertz) ou superior.</p>
+								<p><b>Memória:</b> 2 GB(GigaBytes) de memoria RAM ou superior.</p>
+								<p><b>Espaço Disponível em Disco:</b> 5 GB(GigaBytes) Livres de espaço em disco ou superior.</p>
+								<p><b>Velocidade de Internet:</b>  Internet banda larga com velocidade de 1 megabit (taxa de transferência disponível para download de 200 kilobits /seg).</p>
+								<a href="https://medsoft-desktop-update.s3.amazonaws.com/medsoft-pro.dmg" class="btn_informacoesTecnicas">Download</a>
+							</div>
+							<!-- Fim Panel: macOS -->
+	
+							<!-- Panel: Dispositivos Moveis -->
+							<div class='panel'>
+								<h4>REQUISITOS MÍNIMOS</h4>
+								<hr>
+								<p>Todo o conteúdo acadêmico pertinente ao módulo on-line <b>RAC iMED</b> será oferecido exclusivamente no formato ELETRÔNICO, através do aplicativo MEDSoft Pro, para SMARTPHONES, TABLETS.</p>
+								
+								<p><b>iOS:</b> Sistema operacional com versão superior a 12.0.0</p>
+								<p><b>Android:</b> Sistema operacional com versão superior a 8.0.0</p>
+	
+								<div class="box__botoes">
+									<div class="coluna">
+										<a href="https://itunes.apple.com/br/app/medsoft-pro/id1216235855?mt=8" class="btn_informacoesTecnicas">Download iOS</a>
+									</div>
+								  
+										<div class="coluna">
+										<a href="https://play.google.com/store/apps/details?id=com.medgrupo.medsoft.pro&amp;hl=pt_BR" class="btn_informacoesTecnicas">Download Android</a>
+									</div>
+								</div>
+	
+							</div>
+							<!-- Fim Panel: Dispositivos Moveis -->
+	
+						  </div>
+						  <!-- Fim Panels -->
+						</div>
+						<!-- Fim Tabs -->
+					</div>
+				</div>
+	
+	
+	
+			</div>
+	
+		</section>
+	
+	
+	
+	
+	</div>
+	
+	<div class="center-mobile">
+		<div class="button button--register">
+			<a href="https://medycorp.com.br/medeletro/#/perfil-aluno" target="_blank" class="button__text">inscreva-se</a>
+		</div>
+	</div>
+	
+	</body>
+	
+
+</body>
+
+
+
+
+<script>
+	
+(function() {
+  $(function() {
+    var toggle;
+    return toggle = new Toggle('.toggle');
+  });
+
+  this.Toggle = (function() {
+    Toggle.prototype.el = null;
+
+    Toggle.prototype.tabs = null;
+
+    Toggle.prototype.panels = null;
+
+    function Toggle(toggleClass) {
+      this.el = $(toggleClass);
+      this.tabs = this.el.find(".tab");
+      this.panels = this.el.find(".panel");
+      this.bind();
+    }
+
+    Toggle.prototype.show = function(index) {
+      var activePanel, activeTab;
+      this.tabs.removeClass('active');
+      activeTab = this.tabs.get(index);
+      $(activeTab).addClass('active');
+      this.panels.hide();
+      activePanel = this.panels.get(index);
+      return $(activePanel).show();
+    };
+
+    Toggle.prototype.bind = function() {
+      var _this = this;
+      return this.tabs.unbind('click').bind('click', function(e) {
+        return _this.show($(e.currentTarget).index());
+      });
+    };
+
+    return Toggle;
+
+  })();
+
+}).call(this);
+
+</script>
+
+
+
+<?php get_footer(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
